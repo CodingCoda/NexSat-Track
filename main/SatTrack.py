412a6ae222a763ea5d7c307095d76d5c3c0e0ff7
Dmitri Shevchenko <dmitri.shevchenko.au@gmail.com>
2019-10-14 18:50:51 +1100

    tried adding speed guess

--- main/SatTrack.py
+++ main/SatTrack.py
@@ -7,9 +7,10 @@ import atexit, os
 from colorama import Fore, Back, Style
 
 pi=3.141952
-speedcap = 30 #35 is recommended max speed for 6SE 6th gen mount.
+speedcap = 15 #35 is recommended max speed for 6SE 6th gen mount.
 refreshrate = .5
-
+daylightsaving = 0#-1 #	-1 is on, 0 is off. NOTE: might be broken so leave at 0
+timechange = [0,0,0,0] # change time by date, hour, minute, second
 comport = "COM4" #TODO Auto-detect port
 ser = serial.Serial(comport)
 
@@ -31,20 +32,20 @@ home.lon = '151.2218624'
 home.lat = '-33.7805312'
 home.elevation = 40
 
-TLE1 = 'COSMOS 2058'
-TLE2 = "1 20465U 90010A   19282.14924744  .00000184  00000-0  13301-4 0  9999"
-TLE3 = "2 20465  82.4897 156.1243 0014919 336.9664  23.0887 14.97914373611666"
+TLE1 = 'SL-16 R/B'
+TLE2 = "1 26070U 00006B   19285.55197995 +.00000105 +00000-0 +78122-4 0  9996"
+TLE3 = "2 26070 071.0032 336.2892 0015380 150.0313 210.1694 14.15455829017413"
 
 sat = ephem.readtle(str(TLE1), str(TLE2), str(TLE3))
 
-def AltAzi(y, m, d, h, mi, s): #x=0-360 y=-90-90
+def AltAzi(y, m, d, h, mi, s): # ( x=0-360 y=-90-90 ) Get the curent alt azi of the sat at chosen time
 	home.date = int(y), int(m), int(d), int(h), int(mi), int(s)
 	sat.compute(home)
 	alt = sat.alt * degrees_per_radian
 	azi = sat.az * degrees_per_radian
 	return alt, azi
 
-def AltAziNow(): #Get te cuurent alt azi of the sat at current time
+def AltAziNow(): #Get the curent alt azi of the sat at current time
 	home.date = datetime.utcnow()
 	sat.compute(home)
 	alt = sat.alt * degrees_per_radian
@@ -52,12 +53,12 @@ def AltAziNow(): #Get te cuurent alt azi of the sat at current time
 	return alt, azi
 
 def setTimeNow(): #QRSTUVWX
-	V = datetime.now().year
+	V = datetime.now().year 
 	T = datetime.now().month
-	U = datetime.now().day
-	Q = datetime.now().hour
-	R = datetime.now().minute
-	S = datetime.now().second
+	U = datetime.now().day + timechange[0]
+	Q = datetime.now().hour + timechange[1]
+	R = datetime.now().minute + timechange[2]
+	S = datetime.now().second + + timechange[3]
 	W = 256 - 10
 	X = 0	   #0 standard, 1 daylight savings
 	command = ('H' + chr(Q) + chr(R) + chr(S) + chr(T) + chr(U) + chr(V) + chr(W) + chr(X))
@@ -69,6 +70,7 @@ def setLocationHere(): #151.13.19 S , -33.46.50 E
 	B = 46
 	C = 50
 	D = 1
+
 	E = 151
 	F = 13
 	G = 19
@@ -138,7 +140,7 @@ def getAltAziTelescope(): # x=0-360 and y=0-360
 		quit()
 
 def testTLEAge(TLE2):
-	day_of_year = datetime.now().timetuple().tm_yday
+	day_of_year = datetime.now().timetuple().tm_yday + timechange[0]
 	ISSTLEAge = str(TLE2.split(" ")[5])
 	ISSTLEAge = ISSTLEAge[2:5]
 	if day_of_year - float(ISSTLEAge) > 7:
@@ -148,33 +150,31 @@ def testTLEAge(TLE2):
 def findPass(): #Find next pass (within the day)
 	year = int(datetime.utcnow().year)
 	month = int(datetime.utcnow().month)
-	day = int(datetime.utcnow().day)
-	hour = int(datetime.utcnow().hour)
+	day = int(datetime.utcnow().day) + timechange[0]
+	hour = int(datetime.utcnow().hour+daylightsaving) + timechange[1]
+	maxSpeed = 0
 
 	#Next pass on same day
 	for h in range(hour, 24):
 		for mi in range(0, 60):
 			for s in range(0, 60):
 				x, y = AltAzi(year, month, day, h, mi, s)
+				x1, y1 = AltAzi(year, month, day, h, mi, s + 1)
+				
+				xspeed = abs(x-x1)
+				yspeed = abs(y-y1)
+
+				if xspeed > yspeed:
+					if xspeed > maxSpeed:
+						maxSpeed = xspeed
+				else:
+					if yspeed > maxSpeed:
+						maxSpeed = yspeed
+
 				if x > 0 and h > hour:
-					return False, day, h, mi, s, y
+					return day, h, mi, s, y, maxSpeed
 
-	#Next pass for next day  (will crash if on next month; time is hard)
-	day += 1
-	for h in range(0, 24):
-		for mi in range(0, 60):
-			for s in range(0, 60):
-				x, y = AltAzi(year, month, day, h, mi, s)
-				if x > 0:
-					return True, day, h, mi, s, y
 	return False
-
-
-def debugFlag(state, msg):
-	if state:
-		print(str(msg))
-	else:
-		print("ERROR! \t" + str(msg))
 
 os.system("cls")
 
@@ -196,11 +196,14 @@ if findPass() == False:
 	print("You may try changing location, time, or TLE data to fix this issue...")
 	quit()
 else:
-	cort, day, h, mi, s, y = findPass()
-	if cort == False:
-		print("Next pass found at UTC " + str(h) + ":" + str(mi) + ":" + str(s) + " @ " + str(round(y)))
-	else:
-		print("Next pass found at UTC " + str(h) + ":" + str(mi) + ":" + str(s) + " @ " + str(round(y)) + " tomorrow")
+	day, h, mi, s, y, maxSpeed = findPass()
+	h += daylightsaving
+	print("Next pass found at UTC " + str(h) + ":" + str(mi) + ":" + str(s) + " @ " + str(round(y)))
+
+if maxSpeed > speedcap:
+	print("The max flyby speed of " + str(TLE1) + " (" + str(round(maxSpeed)) + " d/s) will be too fast for the telescope!!!")
+else:
+	print("The max flyby speed of " + str(TLE1) + " (" + str(round(maxSpeed)) + " d/s) should be slow enough for the telescope.")
 
 print(" ")
 print("Connecting to telescope...")
@@ -223,13 +226,13 @@ b = time.time()
 a = time.time()
 
 while 1:
-	timelag = abs(b-a-1)
+	timelag = abs(b-a-refreshrate)
 	a = time.time()
 	os.system("cls")
 	
 	y, x = getAltAziTelescope()
 	
-	y1, x1 = AltAzi(datetime.utcnow().year, datetime.utcnow().month, datetime.utcnow().day, datetime.utcnow().hour, datetime.utcnow().minute, datetime.utcnow().second+refreshrate+timelag)
+	y1, x1 = AltAzi(datetime.utcnow().year, datetime.utcnow().month, datetime.utcnow().day + timechange[0], datetime.utcnow().hour+daylightsaving+ timechange[1], datetime.utcnow().minute+ timechange[2], datetime.utcnow().second+ timechange[3]+refreshrate+timelag)
 	
 	#fixing telescope map to match telescope map
 	if y > 90 and y < 180:
